9752dc9fd6aafbf9759510b0a7d8ec7110e15de8
Andrew Guard <andrew.s.guard@gmail.com>
2014-02-05 12:31:51 -0600

    accepting .txt file

---
+++ MarsRover.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class MarsRover {
+  public static void main(String[] args) {
+
+    File coordinatesFile = new File("mars_rover_input.txt");
+    // Scanner sc = new Scanner(coordinatesFile);
+    // int x = sc.nextInteger();
+    // int y = sc.nextInteger();
+
+    System.out.println(coordinatesFile);
+
+  }
+}

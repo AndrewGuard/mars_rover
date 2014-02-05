b228ba12d598c4b01a5342c1bfc113646674eef4
Andrew Guard <andrew.s.guard@gmail.com>
2014-02-05 13:23:31 -0600

    parsing a bit

--- MarsRover.java
+++ MarsRover.java
@@ -1,14 +1,20 @@
 import java.io.*;
+import java.util.*;
 
 public class MarsRover {
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws FileNotFoundException {
     File coordinatesFile = new File("mars_rover_input.txt");
-    // Scanner sc = new Scanner(coordinatesFile);
-    // int x = sc.nextInteger();
-    // int y = sc.nextInteger();
+    Scanner scan = new Scanner(coordinatesFile);
+    String s = scan.nextLine();
 
-    System.out.println(coordinatesFile);
+    try {
+      Integer.parseInt(s);
+    }
+    catch(NumberFormatException ex){
+      System.out.println("Its not a valid Integer");
+    }
 
+    System.out.println(s);
   }
 }
+

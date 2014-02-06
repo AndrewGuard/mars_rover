a9fbf802254942fe45498924f1a5eee62fcf50cf
Andrew Guard <andrew.s.guard@gmail.com>
2014-02-05 21:14:38 -0600

    parse by line

--- MarsRover.java
+++ MarsRover.java
@@ -1,29 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class MarsRover {
 
   private static void readFile(String fileName) {
-    try {
-      File file = new File(fileName);
-      Scanner scanner = new Scanner(file);
-      while (scanner.hasNext()) {
-        System.out.println(scanner.next());
-      }
-      scanner.close();
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-  }
+   try {
+     Scanner scanner = new Scanner(new File(fileName));
+     scanner.useDelimiter(System.getProperty("line.separator"));
+     while (scanner.hasNext()) {
+       System.out.println(scanner.next());
+     }
+       scanner.close();
+     } catch (FileNotFoundException e) {
+       e.printStackTrace();
+     }
+   }
 
-  public static void main(String[] args) {
-    if (args.length != 1) {
-      System.err.println("usage: java TextScanner1"
-        + "file location");
-      System.exit(0);
-    }
-    readFile(args[0]);
-  }
-}
-
+   public static void main(String[] args) {
+     if (args.length != 1) {
+       System.err.println("usage: java TextScanner1"
+         + "file location");
+       System.exit(0);
+     }
+     readFile(args[0]);
+   }
+ }

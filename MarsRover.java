a3f2668171009f896ffb65d9fcd216997aeff7a9
Andrew Guard <andrew.s.guard@gmail.com>
2014-02-05 20:46:19 -0600

    changed .txt input to command line argument

--- MarsRover.java
+++ MarsRover.java
@@ -1,20 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MarsRover {
-  public static void main(String[] args) throws FileNotFoundException {
-    File coordinatesFile = new File("mars_rover_input.txt");
-    Scanner scan = new Scanner(coordinatesFile);
-    String s = scan.nextLine();
 
+  private static void readFile(String fileName) {
     try {
-      Integer.parseInt(s);
+      File file = new File(fileName);
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNext()) {
+        System.out.println(scanner.next());
+      }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
     }
-    catch(NumberFormatException ex){
-      System.out.println("Its not a valid Integer");
-    }
+  }
 
-    System.out.println(s);
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      System.err.println("usage: java TextScanner1"
+        + "file location");
+      System.exit(0);
+    }
+    readFile(args[0]);
   }
 }
 
